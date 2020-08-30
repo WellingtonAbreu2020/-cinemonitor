da487f7a803e9947da65d132f33beacacf2a0be5
WellingtonAbreu <wellington.abreu2020@outlook.com>
2020-08-30 17:52:31 +0000

    Initial commit

---
+++ cinema.java
@@ -0,0 +1,29 @@
+class cinema{
+  private String titulo;
+  private int ano;
+  private int tipo;
+
+public void setTitulo(String n){
+  titulo = n;
+}
+
+public void setAno (int i){
+  ano = i;
+}
+
+public void setTipo (int a){
+tipo = a;
+}
+ 
+public String getTitulo (){
+  return titulo;
+}
+
+public int getAno(){
+  return ano;
+}
+
+public int getTipo(){
+  return ano;
+}
+}
